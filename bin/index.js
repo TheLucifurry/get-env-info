97095747e24e395d8b4e198a2c858216626dd361
Alexemarg <alexemarg.dev@gmail.com>
2021-06-17 12:19:14 +0700

    1.0.3 - add colored output

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,26 @@
 const { version, release } = require('os');
 const { execSync } = require('child_process');
 
-const os = `${version()} ${release()}`;
-const nodejs = `Node.js ${process.version}`;
-const npm = `NPM v${execSync('npm -v')}`;
+function dye(text, colorCode = '\x1b[0m') {
+  return `${colorCode}${text}\x1b[0m`;
+}
 
-console.log(`\nEnvironment:\n\t${os}\n\t${nodejs}\n\t${npm}\n`);
+const info = {
+  name: {
+    os: version(),
+    node: 'Node.js',
+    npm: 'NPM',
+  },
+  version: {
+    os: release(),
+    node: process.version.replace(/^v/, ''),
+    npm: execSync('npm -v'),
+  }
+};
+
+const result = `
+${dye(info.name.os, '\x1b[36m')} ${dye(info.version.os)}
+${dye(info.name.node, '\x1b[32m')} ${dye(info.version.node)}
+${dye(info.name.npm, '\x1b[31m')} ${dye(info.version.npm)}`;
+
+console.log(result);
